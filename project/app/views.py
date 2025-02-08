36aea3a1b3abf5f66ad6c22eb76fe4147ed9cfd4
Pragati Gupta <pragatigupta0104@gmail.com>
2025-02-08 23:34:56 +0530

    college dashbord

---
+++ project/app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from .models import Student
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+def register(request):
+    if request.method == 'POST':
+        print(request.POST)
+        print(request.FILES)
+        name=request.POST.get('username')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        dob=request.POST.get('dob')
+        subscribe=request.POST.getlist('subscribe')
+        gender=request.POST.get('gender')
+        password=request.POST.get('password')
+        cpassword=request.POST.get('cpassword')
+        profile_pic=request.POST.get('profile-pic')
+        print(name,email,phone,dob,subscribe,gender,password,cpassword,profile_pic)
+        user = Student.objects.filter(email=email)
+        if user:
+            x = "Email already exist"
+            return render(request, 'profile.html', {'msg': x})
+        else:
+            pass
+            if password==cpassword:
+                user = Student.objects.create(name=name, email=email, phone=phone,dob=dob,subscribe=subscribe,gender=gender,password=password,profile_pic=profile_pic)
+                x="Regisration Successfully"
+                return render(request,'login.html',{'msg':x})
+            else:
+                x="Password and Confirm Password does not match"
+                return render(request,'profile.html',{'msg':x,'name':name, 'email':email,'phone':phone,'dob':dob,'subscribe':subscribe,'gender':gender,'profile_pic':profile_pic,})
+        
+    else:
+            return render(request, 'profile.html')
+
