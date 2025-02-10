efdd3f342bca5fe1d8fa642fae6a81a83f65b9ca
Pragati Gupta <pragatigupta0104@gmail.com>
2025-02-10 10:28:47 +0530

    pro

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length = 50)
+    email=models.EmailField()
+    phone=models.IntegerField()
+    subscribe=models.CharField(max_length = 50)
+    gender=models.CharField(max_length = 50)
+    dob=models.DateField()
+    profile_pic=models.ImageField(upload_to = 'image/')
+    password=models.CharField(max_length = 50)
+    
+    def __str__(self):
+        return self.name
--- project/app/urls.py
+++ project/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('',views.home,name='home'),
+    path('',views.index,name='index'),
     path('about/',views.about,name='about'),
     path('registration/',views.registration,name='registration'),
-    path('login/',views.login,name='login')
+    path('login/',views.login,name='login'),
+    
 ]
